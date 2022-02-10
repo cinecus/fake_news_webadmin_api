22855065be506b176404d1bd766bc30a164d32a1
Chananon Chantaratin <chananon.seen@gmail.com>
2022-02-10 23:07:02 +0700

    edit router

--- src/api/index.js
+++ src/api/index.js
@@ -12,12 +12,12 @@ exports.createApi = async (app, version = '/api/v1') => {
     const appendFile = promisify(fs.appendFile)
 
     const folders = (await readdir('./src/api')).filter((f) => !f.includes('.'))
-    await writeFile('./src/api/router.md', '## ROUTER ##')
+    // await writeFile('./src/api/router.md', '## ROUTER ##')
 
     for (const e of folders) {
         const p = path.join(__dirname, `./${e}/${e}Router.js`)
         if (fs.existsSync(p)) {
-            appendFile('./src/api/router.md', `\n \t${version}/${prefix}/${e}`)
+            // appendFile('./src/api/router.md', `\n \t${version}/${prefix}/${e}`)
             app.use(version + `/${prefix}` + `/${e}`, require(`./${e}/${e}Router`))
         }
     }
--- src/api/router.md
+++
@@ -1,4 +0,0 @@
-## ROUTER ##
- 	/api/v1/fncadmin/article
- 	/api/v1/fncadmin/test
- 	/api/v1/fncadmin/auth
